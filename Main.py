df6555139732fbfa91a8e67f52587d8a423b377d
Sheikmo2425 <sheikmo2425v@gmail.com>
2023-11-30 20:00:14 +0530

    2023-11-30

--- Main.py
+++ Main.py
@@ -139,6 +139,6 @@ def out3():
     return ("saved successfully")
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=5000)
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0',port=5000)
